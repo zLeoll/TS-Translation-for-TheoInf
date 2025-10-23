68c450d8bbe1a61f31fc7126d8ca677ecc1691f9
zLeoll <168082030+zLeoll@users.noreply.github.com>
2025-10-23 16:36:31 +0200

    translate Radix-Sort.jpynb

---
+++ Algorithms/TypeScript/Chapter-6/Counting-Sort.ts
@@ -0,0 +1,28 @@
+export type Student = [string, number];
+
+export function countingSort(A: Student[]): Student[];
+export function countingSort<T>(A: [T, number][]): [T, number][];
+
+export function countingSort<T>(A: [T, number][], maxKey = 255): [T, number][] {
+    const Counts = new Array<number>(maxKey + 1).fill(0);
+    const Index = new Array<number>(maxKey + 1).fill(0);
+    const Sorted = new Array<[T, number]>(A.length);
+
+    for (const [, key] of A) {
+        if (key < 0 || key > maxKey) throw new Error(`key out of range: ${key}`);
+        Counts[key]++;
+    }
+
+    Index[0] = 0;
+    for (let k = 0; k < maxKey; k++) {
+        Index[k + 1] = Index[k] + Counts[k];
+    }
+
+    for (const [val, key] of A) {
+        const i = Index[key];
+        Sorted[i] = [val, key];
+        Index[key]++;
+    }
+
+    return Sorted;
+}
